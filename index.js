dd1d9fb4ad2fed318d5cb06c378d14f2aea9ecd1
Tak <takeshiseodev@gmail.com>
2023-12-07 20:19:45 -0500

    update: final

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
+/** @format */
+
 const wishlist = [
-  "Macbook Air M2"
-  // TODO: Add more items here
-];
+    'Self-tying bow ties',
+    'Invisible snowman kit',
+    'Eggnog fountain',
+    'Reindeer games rulebook',
+    "Santa's sleigh GPS",
+    'Cookie-flavored toothpaste',
+    'Unmeltable snowballs',
+    'Holiday jingle doorbell',
+    'Mistletoe drone',
+    'Gingerbread house blueprint',
+]
+
+const wishlistEl = document.querySelector('.wishlist')
+
+wishlist.map(item => {
+    const li = document.createElement('li')
+    li.textContent = item
+    wishlistEl.appendChild(li)
+})
+
+
 
 /** Challenge: 
   - Iterate over the wishlist array.
   - Dynamically render your wishlist from the array.
   - Style the wishlist with CSS.
 **/
+
